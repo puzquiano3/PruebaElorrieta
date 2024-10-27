be3c481a8351aa51e45e444432e3b0dedb57d50d
puzquiano3 <pauzsk8@gmail.com>
2024-10-27 19:28:58 +0100

    controlador

---
+++ Vending/src/controlador/GestorProductos.java
@@ -0,0 +1,49 @@
+package controlador;
+
+import modelo.Productos;
+
+public class GestorProductos {
+	
+	public static int numProductosTipo(int tipo) {
+		int cont=0;
+		for(int n=0;n<Productos.tipos.length;n++) {
+			if(Productos.tipos[n]== tipo) cont++;
+		}
+		return cont;
+	}
+	public static String[] getProductosTipo(int tipo) {
+		int cantidad= numProductosTipo(tipo);
+		if (cantidad>0) {
+			String[] productos=new String[cantidad];
+			int cont=0;
+			for(int n=0;n<Productos.nombres.length;n++) {
+				if(Productos.tipos[n]==tipo) {
+					productos[cont]=Productos.nombres[n];
+					cont++;				
+				}
+			}
+			return productos;
+		
+	}
+		
+		
+		return null;
+	}
+	public static double[] getPreciosTipo(int tipo) {
+		int cantidad= numProductosTipo(tipo);
+		if (cantidad>0) {
+			double[] precios=new double[cantidad];
+			int cont=0;
+			for(int n=0;n<Productos.precios.length;n++) {
+				if(Productos.tipos[n]==tipo) {
+					precios[cont]=Productos.precios[n];
+					cont++;				
+				}
+			}
+			
+		
+	}
+		
+		return null;
+	}
+}
