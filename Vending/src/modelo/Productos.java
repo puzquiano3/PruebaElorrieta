7941c01c0859d0c61e1117827df302ebb0c67fad
puzquiano3 <pauzsk8@gmail.com>
2024-10-27 19:38:57 +0100

    imagenes

--- Vending/src/modelo/Productos.java
+++ Vending/src/modelo/Productos.java
@@ -10,7 +10,8 @@ public class Productos {
 		public static String[] nombres= {"Coca Cola","Fanta","7Up","Patatas","Chaskis","Pajitas","Ositos","Perlas","Boolycao","Donuts","","","","","",""};	
 		public static int tipos[]=       {BEBIDA,    BEBIDA,  BEBIDA,SNACK,   SNACK,    SNACK,   GOMINOLA, GOMINOLA,BOLLERIA,BOLLERIA,0,0,0,0,0,0};	
 		public static double[] precios= {1.25        ,1.2    ,1.2    ,1.0,    0.9,       0.85,    0.95,     0.90,    1.00 ,     1.05 ,0,0,0,0,0,0};
-		public static int[] identificador = {1,2,3,4,5,6,7,8,9,10,-1,-1,-1,-1,-1,-1};
+		public static int[] identificador = {0,1,2,3,4,5,6,7,8,9,10,-1,-1,-1,-1,-1};
+		public static String[] imagen= {"img0.png","img1.png","img2.png","img2.png","img3.png","img4.png","img5.png","img6.png","img7.png","img8.png","img9.png","noimg.png","noimg.png","noimg.png","noimg.png","noimg.png"};
 		
 		
 		
